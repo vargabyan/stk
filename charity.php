7b3876ec2b84d301186391de171d1448dc0d1353
vargabyan <vardan.gabrielyan.it@gmail.com>
2023-10-03 13:17:46 +0500

    #1

---
+++ charity.php
@@ -0,0 +1,69 @@
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<link rel="stylesheet" href="style.css">
+
+
+<div class="core-container" style="margin-top: 130px">
+
+    <section class="breadcrumb">
+        <p class="breadcrumb_home">Главная</p>
+        <p class="breadcrumb_item">Баготворительность</p>
+    </section>
+
+    <section class="charity-program">
+        <div class="charity-program_description-wrapper">
+            <p class="charity-program_header">Благотворительная программа</p>
+            <p class="charity-program_description">Мы развиваем культуру благотворительности, объединяя вокруг себя активных творческих профессионалов, которые участвуют в решении общественно значимых задач и добиваются устойчивых социальных изменений. Мы открываем возможности для появления новых идей и создаем условия для их воплощения.</p>
+        </div>
+        <img class="charity-program_img" src="/images/Rectangle%201650.png" alt="">
+    </section>
+
+    <section class="activity">
+        <div class="activity_header-wrapper">
+            <p class="activity_header">Деятельность </p>
+            <a class="activity_link-all-events" href="">Все события</a>
+        </div>
+
+        <ul class="activity_teg">
+            <li class="activity_teg_item">последние новости</li>
+        </ul>
+
+        <div class="activity_events-wrapper">
+            <div class="activity_events_item item-1">
+                <div class="activity_img-wrapper">
+                    <div class="activity_date">28 июля</div>
+                    <img class="activity_events_img" src="/images/Platform-6_00016-1024x683%20(1)%203.png" alt="">
+                </div>
+                <div class="activity_events_description-wrapper">
+                    <p class="activity_events_header">Mi suspendisse interdum nec scelerisque neque amet id. Ac tortor.</p>
+                    <p class="activity_events_description">Proin dolor pharetra sem posuere vel enim. Auctor nullam tristique consectetur enim ornare at. Faucibus molestie auctor eu consequat in. Ornare gravida vitae habitasse ante cras.</p>
+                </div>
+            </div>
+
+            <div class="activity_events_item item-2">
+                <div class="activity_img-wrapper">
+                    <div class="activity_date">28 июля</div>
+                    <img class="activity_events_img" src="/images/Platform-6_00016-1024x683%20(1)%203.png" alt="">
+                </div>
+                <div class="activity_events_description-wrapper">
+                    <p class="activity_events_header">Mi suspendisse interdum nec scelerisque neque amet id. Ac tortor.</p>
+                    <p class="activity_events_description">Proin dolor pharetra sem posuere vel enim. Auctor nullam tristique consectetur enim ornare at. Faucibus molestie auctor eu consequat in. Ornare gravida vitae habitasse ante cras.</p>
+                </div>
+            </div>
+
+            <div class="activity_events_item item-3">
+                <div class="activity_img-wrapper">
+                    <div class="activity_date">28 июля</div>
+                    <img class="activity_events_img" src="/images/Platform-6_00016-1024x683%20(1)%203.png" alt="">
+                </div>
+                <div class="activity_events_description-wrapper">
+                    <p class="activity_events_header">Mi suspendisse interdum nec scelerisque neque amet id. Ac tortor.</p>
+                    <p class="activity_events_description">Proin dolor pharetra sem posuere vel enim. Auctor nullam tristique consectetur enim ornare at. Faucibus molestie auctor eu consequat in. Ornare gravida vitae habitasse ante cras.</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+</div>
+
